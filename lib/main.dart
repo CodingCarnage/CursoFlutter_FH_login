997a24874f4d2b31a850e8fca6808a8847e8cb37
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-01-05 00:47:26 -0600

    Se cambio la fuente predeterminada de la app.

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'package:login/src/pages/home_page.dart';
 import 'package:login/src/pages/login_page.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,9 +13,12 @@ class MyApp extends StatelessWidget {
       title: 'Material App',
       initialRoute: 'login',
       routes: {
-        'login' : (BuildContext context) => LoginPage(),
-        'home' : (BuildContext context) => HomePage(),
+        'login': (BuildContext context) => LoginPage(),
+        'home': (BuildContext context) => HomePage(),
       },
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+      ),
     );
   }
 }

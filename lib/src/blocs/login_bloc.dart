c7fe18b899daae08f6512cf1377ffc00335de9c7
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-01-05 21:16:35 -0600

    Correción el tipo de dato y poder obtener el ultimo valor de mi stream.

--- lib/src/blocs/login_bloc.dart
+++ lib/src/blocs/login_bloc.dart
@@ -1,18 +1,27 @@
 import 'dart:async';
 
+import 'package:rxdart/rxdart.dart';
+
 import 'package:login/src/blocs/validators.dart';
 
+
 class LoginBloc with Validators {
-  final StreamController _emailController = StreamController<String>.broadcast();
-  final StreamController _passwordController = StreamController<String>.broadcast();
+  final BehaviorSubject _emailController = BehaviorSubject<String>();
+  final BehaviorSubject _passwordController = BehaviorSubject<String>();
   
   // Get values from the stream.
   Stream<String> get emailStream => _emailController.stream.transform(validateEmail);
   Stream<String> get passwordStream => _passwordController.stream.transform(validatePassword);
 
+  Stream<bool> get fromValidStream => Rx.combineLatest2(emailStream, passwordStream, (e, p) => true);
+  
   // Set values to the stream.
   Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changePassword => _passwordController.sink.add;
+
+  // Get last value from stream.
+  String get email => _emailController.value;
+  String get password => _passwordController.value;
 
   void dispose() { 
     _emailController?.close();

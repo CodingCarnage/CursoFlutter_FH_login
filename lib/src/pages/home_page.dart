15b0ace41558b5970294297c6a72fef3e734c3ce
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-01-05 21:18:21 -0600

    Se puede visualizar la contraseña que ingresamos en el login. Los datos si se pasan.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 
+import 'package:login/src/blocs/provider.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final bloc = Provider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Email: ${bloc.email}'),
+            Divider(),
+            Text('Password: ${bloc.password}')
+          ],
+        ),
       ),
     );
   }

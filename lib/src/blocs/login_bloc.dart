f6a7daed629182fe5a321318333bc3af804463de
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-01-05 20:19:59 -0600

    Se implementa la validación en el stream

--- lib/src/blocs/login_bloc.dart
+++ lib/src/blocs/login_bloc.dart
@@ -1,12 +1,14 @@
 import 'dart:async';
 
-class LoginBloc {
+import 'package:login/src/blocs/validators.dart';
+
+class LoginBloc with Validators {
   final StreamController _emailController = StreamController<String>.broadcast();
   final StreamController _passwordController = StreamController<String>.broadcast();
   
   // Get values from the stream.
-  Stream<String> get emailStream => _emailController.stream;
-  Stream<String> get passwordStream => _passwordController.stream;
+  Stream<String> get emailStream => _emailController.stream.transform(validateEmail);
+  Stream<String> get passwordStream => _passwordController.stream.transform(validatePassword);
 
   // Set values to the stream.
   Function(String) get changeEmail => _emailController.sink.add;

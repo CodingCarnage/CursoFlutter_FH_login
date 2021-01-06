30c18fdb25c0b0b837675d63d3346afe3aecfe10
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-01-06 17:20:37 -0600

    Se agrego un FloatingActionButton para navegar a la pagina de productos.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,28 +1,24 @@
 import 'package:flutter/material.dart';
 
-import 'package:login/src/blocs/provider.dart';
-
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final LoginBloc bloc = Provider.of(context);
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Page'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Email: ${bloc.email}'),
-            Divider(),
-            Text('Password: ${bloc.password}')
-          ],
-        ),
-      ),
+      body: Container(),
+      floatingActionButton: _createFloatingActionButton(context),
+    );
+  }
+
+  FloatingActionButton _createFloatingActionButton(BuildContext context) {
+    return FloatingActionButton(
+      child: Icon(Icons.add),
+      onPressed: () => Navigator.pushNamed(context, 'product'),
     );
   }
 }

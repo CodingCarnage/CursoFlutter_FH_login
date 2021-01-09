b8fb26879ace099e2fa8cdc382a6bf60b08beea3
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-01-09 15:22:47 -0600

    Se agrego un modelo para los productos.

---
+++ lib/src/models/product_model.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+ProductModel productModelFromJson(String str) => ProductModel.fromJson(json.decode(str));
+
+String productModelToJson(ProductModel data) => json.encode(data.toJson());
+
+class ProductModel {
+    ProductModel({
+        this.id,
+        this.title = '',
+        this.value = 0.0,
+        this.available = true,
+        this.imageUrl,
+    });
+
+    String id;
+    String title;
+    double value;
+    bool available;
+    String imageUrl;
+
+    factory ProductModel.fromJson(Map<String, dynamic> json) => ProductModel(
+        id: json["id"],
+        title: json["title"],
+        value: json["value"].toDouble(),
+        available: json["available"],
+        imageUrl: json["imageUrl"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "value": value,
+        "available": available,
+        "imageUrl": imageUrl,
+    };
+}

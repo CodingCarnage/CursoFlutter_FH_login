69e1afa6314bf918156c8dff78c997edf373f57c
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-01-09 21:31:21 -0600

    Se creo una función para crear Products.

--- lib/src/providers/product_provider.dart
+++ lib/src/providers/product_provider.dart
@@ -1,3 +1,21 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'package:login/src/models/product_model.dart';
+
 class ProductProvider {
-  
+  final String _url = 'https://flutter-project-e22eb-default-rtdb.firebaseio.com';
+
+  Future<bool> createProduct(ProductModel product) async {
+    final String url = '$_url/products.json';
+    
+    final http.Response resp = await http.post(url, body: productModelToJson(product));
+
+    final decodedData = json.decode(resp.body);
+
+    print(decodedData);
+
+    return true;
+  }
 }
